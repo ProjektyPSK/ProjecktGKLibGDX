5dc52f227eefd22b4b10f1aca43b2a2154d5b681
Piotr Lepa <piotr-lepa@o2.pl>
2020-04-08 20:12:40 +0200

    prawie bez różnicy

--- core/src/screen/PlayScreen.java
+++ core/src/screen/PlayScreen.java
@@ -6,9 +6,14 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -25,6 +30,8 @@ public class PlayScreen implements Screen {
     private OrthogonalTiledMapRenderer renderer;
 
 
+    private World world;
+    private Box2DDebugRenderer b2dr;
     public PlayScreen (Main game){
         this.game=game;
         gamecam= new OrthographicCamera();
@@ -36,6 +43,26 @@ public class PlayScreen implements Screen {
         renderer = new OrthogonalTiledMapRenderer(map);
         gamecam.position.set(gamePort.getWorldWidth() / 2 , gamePort.getWorldHeight()/2 , 0);
 
+        world= new World(new Vector2(0,0), true);
+        b2dr = new Box2DDebugRenderer();
+
+        BodyDef bdef= new BodyDef();
+        PolygonShape shape= new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+
+        // Creata body for borders
+     /*   for(MapObject object : map.getLayers().get(1).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2 , rect.getY() + rect.getHeight() / 2 );
+
+            body = world.createBody(bdef);
+            shape.setAsBox(rect.getWidth() / 2 , rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }*/
     }
     @Override
     public void show() {
@@ -49,6 +76,7 @@ public class PlayScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         renderer.render();
 
+        b2dr.render(world, gamecam.combined);
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
     }
